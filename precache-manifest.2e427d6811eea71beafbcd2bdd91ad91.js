0c5e537b8e49d9ab9f1396d69316331a24068050
Bartek Krupa <krupa1.bk@gmail.com>
2019-07-24 17:53:14 +0200

    Updates

---
+++ precache-manifest.2e427d6811eea71beafbcd2bdd91ad91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393e6995f759bdcf3d76c7ff4cd5cc37",
+    "url": "/candy-shop/index.html"
+  },
+  {
+    "revision": "6a0e3153ce156406dc3c",
+    "url": "/candy-shop/static/css/main.64c8b397.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/candy-shop/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6a0e3153ce156406dc3c",
+    "url": "/candy-shop/static/js/main.ae31435c.chunk.js"
+  },
+  {
+    "revision": "c58c3fd2c5c36c38941d",
+    "url": "/candy-shop/static/js/runtime~main.6db525d3.js"
+  },
+  {
+    "revision": "6dc4e4dcf9b579f018a0d38004508a7c",
+    "url": "/candy-shop/static/media/bee.6dc4e4dc.png"
+  }
+]);
